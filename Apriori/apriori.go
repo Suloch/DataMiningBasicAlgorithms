7bf9ce4f057693537aeefdf8474f2ce0830e58d5
Sumit Patel <sumit.patel.cse19@heritageit.edu.in>
2018-07-27 16:15:38 +0530

    adding the association rule generation

--- Apriori/apriori.go
+++ Apriori/apriori.go
@@ -49,17 +49,70 @@ func main(){
   }
 
 
-//  A, B, AUB := getInput()
+  a, aub := getInput()
+
+  sup_count_a := countSupport(a, transactions)
+  sup_count_aub := countSupport(aub, transactions)
+  fmt.Println("support count of", a, " => ", sup_count_a)
+  fmt.Println("support count of", aub, " => ", sup_count_aub)
+  fmt.Printf("confidence => %.2f percent\n", (float32(sup_count_aub)/float32(sup_count_a))*100)
 
 }
 
 //function to get input from the user
-func getInput() ([]int, []int, []int){
+func getInput() ([]int, []int){
 
-  var A, B, AUB []int
+  var a, b []int
+
+  a = make([]int, 0)
+  b = make([]int, 0)
 
 
-  return A,B,AUB
+  var stringA, stringB  string
+
+  fmt.Println("Enter A:")
+  fmt.Scanf("%s ", &stringA)
+
+  fmt.Println("Enter B:", stringB)
+  fmt.Scanf("%s ", &stringB)
+
+  alist := strings.Split(stringA, ",")
+
+  for _,numstring := range alist{
+      num,err := strconv.Atoi(numstring)
+      if err != nil{
+        fmt.Println(err)
+      }
+
+      a = append(a, num)
+  }
+
+  blist := strings.Split(stringB, ",")
+
+
+    for _,numstring := range blist{
+        num,err := strconv.Atoi(numstring)
+        if err != nil{
+          fmt.Println(err)
+        }
+
+        b = append(b, num)
+    }
+
+    aub := append(a, b...)
+
+    for i := 0; i < len(aub); i++{
+      for j := i+1; j < len(aub);{
+        if aub[i] == aub[j]{
+        aub[j] = aub[len(aub)-1]
+        aub = aub[:len(aub)-1]
+        }else{
+          j++;
+        }
+      }
+    }
+
+  return a,aub
 }
 
 //function to count the support of an item set
@@ -93,7 +146,9 @@ func generateC(L table) table{
 
   for _,row1 := range L.rows{
     for _,row2 := range L.rows{
-      row3, merged := mergeRow(row1, row2)
+      var row3 row
+      var merged bool
+      row3.item_set, merged = mergeRow(row1.item_set, row2.item_set)
       if merged{
         present := false
         for _, row4 := range C.rows{
@@ -131,15 +186,17 @@ func generateC(L table) table{
 
 
 //function to merge two item sets if possible
-func mergeRow(row1 row, row2 row) (row,bool){
-  var row3 row
+func mergeRow(item_set1 []int , item_set2 []int) ([]int,bool){
+  var item_set3 []int
 
-  var check_item = make([]bool, len(row1.item_set))
+  item_set3 = make([]int, 0)
+
+  var check_item = make([]bool, len(item_set1))
   check := 0
 
-  for i,item1 := range row1.item_set{
+  for i,item1 := range item_set1{
     check_item[i] = false
-    for _,item2 := range row2.item_set{
+    for _,item2 := range item_set2{
 
       if(item1 == item2){
         check++
@@ -148,32 +205,46 @@ func mergeRow(row1 row, row2 row) (row,bool){
     }
   }
 
-  if len(row1.item_set) - check == 1{
-    for i,item := range row1.item_set{
+  if len(item_set1) - check == 1{
+    for i,item := range item_set1{
       if !check_item[i]{
-        row3.item_set = append(row3.item_set, item)
+        item_set3 = append(item_set3, item)
       }
     }
 
-    for _,item := range row2.item_set{
-      row3.item_set = append(row3.item_set, item)
+    for _,item := range item_set2{
+      item_set3 = append(item_set3, item)
     }
-    return row3, true
+    return item_set3, true
   }
 
-  return row3, false
+  return item_set3, false
 }
 
 //function to print a table
 func printTable(table1 table){
-    for _,row1 := range table1.rows{
-      for _,item := range row1.item_set{
-        fmt.Printf("%2d ", item)
-      }
-      fmt.Printf("%2d\n", row1.sup_count)
-    }
 
-    fmt.Println("\n\n")
+  if len(table1.rows) == 0{
+    return
+  }
+
+  for i := 0; i <= len(table1.rows[0].item_set); i++{
+    fmt.Printf("----")
+  }
+
+  fmt.Println()
+
+  for _,row1 := range table1.rows{
+    fmt.Printf("|")
+    for _,item := range row1.item_set{
+      fmt.Printf("%2d ", item)
+    }
+    fmt.Printf("|%2d |\n", row1.sup_count)
+  }
+  for i := 0; i <= len(table1.rows[0].item_set); i++{
+    fmt.Printf("----")
+  }
+  fmt.Println("\n")
 }
 
 
